63777e76e477cde875caddfb2d0727c641d9efee
houyunpeng <1600892327@qq.com>
2016-02-25 18:23:24 +0800

    Update push.py

--- push.py
+++ push.py
@@ -32,21 +32,20 @@ def push(app, title, content, type):
     push.send()
 
 
-# Android测试
 android_test = wrap_app('fcec9bf5jas8feg9f1815ef9f', '8c1f23dfo103sdwetoge7bcc')
 
-# IOS测试
+
 ios_test = wrap_app('94d21059osidf966e1a29da', 'e496504oi38f9ahs09qhfa70yq34kj')
 
-# 正式线上，Android和IOS appstroe版本
+
 official = wrap_app('13f39823ojsagea74f4d4d1', '94272sodif9823h9df28ea7')
 
-# IOS企业版
+
 ios_ee = wrap_app('89597203095nasiudcb26', 'bc72247967oiwe0289fasgwl05cf399c')
 
 
-_title = '金联储'
-_content = '猜灯谜，做财迷😍！正月十五来金联储，猜对灯谜送Money💰。让你拿钱拿到手软！'
+_title = 'title'
+_content = 'push content message'
 _type = 'project'
 
 def main():
